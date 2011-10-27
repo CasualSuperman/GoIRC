3f3999cb8c9bc3c0d62a5df20ab45859d802f5ff
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-10-26 21:13:21 -0400

    Removed some unused code.

--- ui.js
+++ ui.js
@@ -1,7 +1,5 @@
 (function (){
 
-var style = document.getElementById("style");
-
 function resize() {
 	var height = window.innerHeight;
 	var offset = document.getElementById("networks").clientHeight;
@@ -11,7 +9,5 @@ window.onresize = resize;
 function ready() {
 	resize();	
 }
-
-// This is the style tag reserved for programmatically changing the width of the name column.
 
 })();
