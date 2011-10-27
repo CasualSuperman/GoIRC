dd90db79170e81fa7bc132a7f5ee8b81d8f12d25
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-10-27 15:24:56 -0400

    Almost got it bottom aligned.
    Also with a scrollbar.

--- index.php
+++ index.php
@@ -4,19 +4,15 @@
 		<meta charset="utf-8">
 		<title>goIRC</title>
 		<!--[if lt IE 9]>
-		<link rel="stylesheet" href="/html5/fixes/html5.css" />
-		<script src="/html5/fixes/html5.js" type="text/javascript"></script>
+			<link rel="stylesheet" href="/html5/fixes/html5.css" />
+			<script src="/html5/fixes/html5.js" type="text/javascript"></script>
+		<![endif]-->
+		<!--[if IE 9]>
+			<style>header #gradient{filter:none}</style>
 		<![endif]-->
 		<link rel="stylesheet" href="/~rwertman/libs/php/font.php?n=Segoe UI&f=Segoe&b" />
 		<link rel="stylesheet" href="main.css" media="screen" />
 		<link rel="stylesheet" href="small.css" media="handheld" />
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.js"></script>
-		<script src="/~rwertman/libs/js/jquery.transition.js"></script>
-		<script src="/~rwertman/libs/js/jquery.color.clo.js"></script>
-		<script src="ui.js"></script>
-<!--		<script src="/libs/js/socket.io.min.js"></script>
-		<script src="ui.js"></script>
-		<script src="app.js"></script>-->
 	</head>
 <!--
 Copyright 2011 Bobby Wertman.
@@ -24,17 +20,18 @@ Copyright 2011 Bobby Wertman.
 <body>
 	<header>
 		<ul id="networks">
-			<li class="network active">foonetic</li>
-			<li class="network nonitem">+connect</li>
+			<li class="active">foonetic</li>
+			<li class="nonitem">+connect</li>
 		</ul>
 		<ul id="rooms">
-			<li class="chat active">#ufeff</li>
-			<li class="chat">#xkcd</li>
-			<li class="chat nonitem">+join</li>
+			<li class="active">#ufeff</li>
+			<li>#xkcd</li>
+			<li class="nonitem">+join</li>
 		</ul>
+		<div id="gradient"></div>
 	</header>
-	<table id="chat">
-		<tbody id="log" valign="bottom">
+	<div id="chat">
+		<table id="log"><tbody>
 			<tr class="status">
 				<td class="author">User</td>
 				<td>
@@ -61,10 +58,9 @@ Copyright 2011 Bobby Wertman.
 				<td class="author op">@s0urc3</td>
 				<td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed quam quam. Vivamus id lectus sit amet libero porta porta. Nullam vulputate eleifend dapibus. Nulla iaculis nibh eu ipsum pretium ultricies eu vitae nunc. Aliquam ullamcorper quam in est dapibus semper. Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac lorem consectetur sapien imperdiet scelerisque. In et convallis tellus. Quisque vitae ligula in mauris vestibulum dapibus. Nam hendrerit congue felis a iaculis. Donec vel neque leo. Donec imperdiet sapien eget magna commodo id ornare mauris pretium. Nam nulla massa, dictum eget lacinia vel, fermentum commodo ipsum. Sed nunc magna, sodales sit amet suscipit non, pellentesque a libero. Praesent nulla sem, adipiscing id elementum ut, volutpat vitae purus. Duis leo est, cursus nec mollis sit amet, suscipit quis leo. Vivamus sodales justo nec odio pulvinar adipiscing. Nulla facilisi. Morbi eget lacus vel nunc tincidunt fringilla in quis tortor. Cras volutpat pretium sem a lobortis. Proin sit amet lacus eu metus rutrum tincidunt at non ligula. Donec eu tellus augue. Vestibulum vitae augue a nibh tincidunt mollis eget eu ipsum. Nam sollicitudin tincidunt nunc adipiscing molestie. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut in feugiat tellus. Phasellus ullamcorper vulputate pellentesque. Donec id lorem augue, a cursus lorem. Integer posuere blandit ornare. Nam venenatis suscipit pretium. Pellentesque a orci in arcu gravida tincidunt pulvinar eu libero. Integer a nisl non neque euismod suscipit nec a arcu.</td>
 			</tr>
-		</tbody>
-	</table>
+		</tbody></table>
+	</div>
 	<input id="talk" autofocus="autofocus" />
-	<style type="text/css" id="style"></style>
 <!--
 Kind of TODO:
 	* Tab autocompletes names
@@ -73,5 +69,9 @@ Kind of TODO:
 	* Max-height on incoming messages area
 	* Also making it actually work.
 -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.js"></script>
+	<script src="/~rwertman/libs/js/jquery.transition.js"></script>
+	<script src="/~rwertman/libs/js/jquery.color.clo.js"></script>
+	<script src="ui.js"></script>
 </body>
 </html>
--- ui.js
+++ ui.js
@@ -1,13 +1,13 @@
 (function (){
 
 function resize() {
-	var height = window.innerHeight;
-	var offset = document.getElementById("networks").clientHeight;
-	document.getElementById("log").style.maxHeight = (height - offset - 25) + "px";
+	var offset = document.getElementsByTagName("header")[0].clientHeight;
+	document.getElementById("chat").style.paddingTop = (offset) + "px";
 }
-window.onresize = resize;
 function ready() {
-	resize();	
+	resize();
 }
+
+ready();
 
 })();
