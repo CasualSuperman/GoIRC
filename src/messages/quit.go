122e865aa51c2e65b9da1b5391ddba0eb8250338
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-10-29 00:51:15 -0400

    Added quit message.

---
+++ src/messages/quit.go
@@ -0,0 +1,16 @@
+package main
+
+type m_quit struct {
+	reason string
+}
+
+func NewQuitMessage(reason string) {
+	return m_quit{reason}
+}
+
+func (m m_quit) String() string {
+	if m.reason == "" {
+		return ":source QUIT :Leaving."
+	}
+	return ":source QUIT :" + m.reason
+}
