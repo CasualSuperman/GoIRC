88b5c89ad6b4e0026590b1448e8af0c983660827
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-10-29 00:48:51 -0400

    More work.

--- src/messages/join.go
+++ src/messages/join.go
@@ -6,7 +6,14 @@ type m_join struct {
 	channel string
 }
 
+func NewJoinMessage(channel string) {
+	return m_join{channel}
+}
+
 func (m m_join) String() string {
+	if m.channel == "" {
+		return ":source JOIN 0"
+	}
 	s := ":source JOIN :"
 	if strings.TrimLeft(m.channel, "#") == m.channel {
 		s += "#" + m.channel
---
+++ src/messages/private.go
@@ -0,0 +1,14 @@
+package main
+
+type m_private struct {
+	target string
+	message string
+}
+
+func NewPrivateMessage(target, message string) m_private {
+	return m_private{target, message}
+}
+
+func (m m_private) String() string {
+	return ":source PRIVMSG " + m.target + " :" + m.message
+}
