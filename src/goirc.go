d627432640a9de8168597526f7983c2824cd9571
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-11-17 16:21:52 -0500

    Websocket server?

--- src/goirc.go
+++ src/goirc.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"http"
+	"websocket"
+)
 
+func main() {
+	http.Handle("/irc", websocket.Handler(Handle))
+	fmt.Println("Handling.")
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		fmt.Println("ListenAndServe failed.")
+	}
+}
+
+func Handle(ws *websocket.Conn) {
+	fmt.Println("New Connection.")
+	fmt.Println(ws.Protocol)
+	fmt.Println(ws.Request)
+	reader := bufio.NewReader(ws)
+	ws.Write([]uint8("Hello, world.\n"))
+	for {
+		fmt.Println(reader.ReadLine())
+	}
+	fmt.Println("Conn closed.")
 }
