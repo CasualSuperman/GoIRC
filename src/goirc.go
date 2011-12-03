1a8da9cf3ee2283539db20dfa660c139642aae57
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-12-02 22:55:05 -0500

    Updated to weekly.

--- src/goirc.go
+++ src/goirc.go
@@ -1,29 +1,46 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
-	"http"
+	"net/http"
 	"websocket"
 )
 
 func main() {
-	http.Handle("/irc", websocket.Handler(Handle))
+	http.Handle("/", websocket.Handler(Handle))
 	fmt.Println("Handling.")
-	err := http.ListenAndServe(":12345", nil)
+	err := http.ListenAndServe(":8088", nil)
 	if err != nil {
 		fmt.Println("ListenAndServe failed.")
+		fmt.Println(err)
 	}
 }
 
 func Handle(ws *websocket.Conn) {
-	fmt.Println("New Connection.")
-	fmt.Println(ws.Protocol)
-	fmt.Println(ws.Request)
-	reader := bufio.NewReader(ws)
-	ws.Write([]uint8("Hello, world.\n"))
-	for {
-		fmt.Println(reader.ReadLine())
+	data := make(map[string]string)
+	done := false
+
+	data["msg"] = "Hello World!"
+
+	fmt.Printf("New Connection from %s.\n", ws.RemoteAddr())
+
+	reader := json.NewDecoder(ws)
+	writer := json.NewEncoder(ws)
+
+	writer.Encode(data)
+
+	for !done {
+		reader.Decode(&data)
+
+		if data["action"] == "disconnect" {
+			done = true
+		} else {
+			fmt.Println(data)
+		}
+
+		data := make(map[string]string)
 	}
 	fmt.Println("Conn closed.")
+	ws.Close()
 }
